75e153200913ddbe8391fc56d2e61da510008be4
Guilherme <guilhermemen2003@gmail.com>
2026-05-28 14:32:09 -0300

    Incrase the refresh token time

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -92,7 +92,7 @@ export class AuthController {
       httpOnly: true,
       secure: true,
       sameSite: 'strict',
-      maxAge: 15 * 60 * 1000,
+      maxAge: 60 * 60 * 1000,
     });
 
     return { user };
